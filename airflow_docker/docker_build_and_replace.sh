aec0e6015237fa61dc08b47892ebddb3b62ea795
Marcin Zieliński <emzet87@gmail.com>
2017-04-21 18:49:57 +0200

    dockerhost changes

--- airflow_docker/docker_build_and_replace.sh
+++ airflow_docker/docker_build_and_replace.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-export DOCKERHOST=$(ifconfig | grep -E "([0-9]{1,3}\.){3}[0-9]{1,3}" | grep -v 127.0.0.1 | awk '{ print $2 }' | cut -f2 -d: | head -n1)
+echo "DOCKERHOST=$(ifconfig | grep -E "([0-9]{1,3}\.){3}[0-9]{1,3}" | grep -v 127.0.0.1 | awk '{ print $2 }' | cut -f2 -d: | head -n1)" > .env
 docker-compose up --build -d --remove-orphans --force-recreate
--- backend_docker/docker_build_and_replace.sh
+++ backend_docker/docker_build_and_replace.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
+echo "DOCKERHOST=$(ifconfig | grep -E "([0-9]{1,3}\.){3}[0-9]{1,3}" | grep -v 127.0.0.1 | awk '{ print $2 }' | cut -f2 -d: | head -n1)" > .env
 docker-compose up --build -d --remove-orphans --force-recreate
